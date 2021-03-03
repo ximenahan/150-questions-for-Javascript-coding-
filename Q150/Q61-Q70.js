085e89f5121c424c07b6646e79533e54581251e2
ximenahan <76551999+ximenahan@users.noreply.github.com>
2021-03-03 13:39:05 +0800

    Update Q61-Q70.js

--- Q150/Q61-Q70.js
+++ Q150/Q61-Q70.js
@@ -103,19 +103,3 @@ alert(" " && 3 && "I" && 1 && true && "There is no false value here.");
 
 
 
-//Q81: use switch, case, break,default method. ask the user to enter a value from 0–2. There are four case, it is 0, 1, 2, and it is out of the range
-
-
-//Q82: same as Q81, but use grouped cases to show more alerts for more of the cases
-
-
-//2.15 Functions
-
-
-//Q83: decalre a function with an alert and call it out
-
-
-//Q84: show A for apple, B for ball, C for cat in one function
-
-
-//Q85: ask the age of the user, and the person is more than 18, then tell one access granted, if no, ask one do you have the permission from the parents, if no then show access deny
