9417d97ef27f63ab72c389e6e490b5e1ede5ab6f
ximenahan <76551999+ximenahan@users.noreply.github.com>
2021-03-03 13:39:36 +0800

    Create Q81-Q90.js

---
+++ Q150/Q81-Q90.js
@@ -0,0 +1,16 @@
+//Q81: use switch, case, break,default method. ask the user to enter a value from 0–2. There are four case, it is 0, 1, 2, and it is out of the range
+
+
+//Q82: same as Q81, but use grouped cases to show more alerts for more of the cases
+
+
+//2.15 Functions
+
+
+//Q83: decalre a function with an alert and call it out
+
+
+//Q84: show A for apple, B for ball, C for cat in one function
+
+
+//Q85: ask the age of the user, and the person is more than 18, then tell one access granted, if no, ask one do you have the permission from the parents, if no then show access deny
